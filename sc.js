12a3e5cc0c4d667e0fe4443be697ac858b779f5c
Codeplus12 <mohitbaghel001@outlook.com>
2024-04-08 09:43:23 +0530

    complete with let

---
+++ sc.js
@@ -0,0 +1,41 @@
+// let,var,const
+
+
+
+
+let a="About Us";
+let b="Amenities"
+let c="Gallery"
+let d="Packages"
+let e="Booking"
+
+let r1="$ 125 / Night"
+let r2="$ 255 / Night"
+let r3="$ 375 / Night"
+let r4="$ 425 / Night"
+
+let d1="+1 800 123 456"
+let d2="info@domain.com"
+let d3="34-D, Greenville, NJ"
+
+
+document.getElementById('m1').innerHTML=a;
+document.getElementById('m2').innerHTML=b;
+document.getElementById('m3').innerHTML=c;
+document.getElementById('m4').innerHTML=d;
+document.getElementById('m5').innerHTML=e;
+
+document.getElementById('h1').innerHTML=a;
+document.getElementById('h2').innerHTML=b;
+document.getElementById('h3').innerHTML=c;
+document.getElementById('h4').innerHTML=d;
+document.getElementById('h5').innerHTML=e;
+
+document.getElementById('m6').innerHTML=r1;
+document.getElementById('m7').innerHTML=r2;
+document.getElementById('m8').innerHTML=r3;
+document.getElementById('m9').innerHTML=r4;
+
+document.getElementById('n1').innerHTML=d1;
+document.getElementById('n2').innerHTML=d2;
+document.getElementById('n3').innerHTML=d3;
